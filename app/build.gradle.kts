1d9552d19a4a3d692fa4e7731942fd335ee5a05f
ismailmesut <ismailmesutmujde@gmail.com>
2024-09-21 09:35:35 +0300

    updated: toast message

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,6 +7,10 @@ android {
     namespace = "com.ismailmesutmujde.kotlintoastmessage"
     compileSdk = 34
 
+    buildFeatures {
+        viewBinding = true
+    }
+
     defaultConfig {
         applicationId = "com.ismailmesutmujde.kotlintoastmessage"
         minSdk = 24
--- app/src/main/java/com/ismailmesutmujde/kotlintoastmessage/MainActivity.kt
+++ app/src/main/java/com/ismailmesutmujde/kotlintoastmessage/MainActivity.kt
@@ -1,13 +1,40 @@
 package com.ismailmesutmujde.kotlintoastmessage
 
 import android.os.Bundle
+import android.view.Gravity
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.ismailmesutmujde.kotlintoastmessage.databinding.ActivityMainBinding
 
 
+@Suppress("DEPRECATION")
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var bindingMain : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        bindingMain = ActivityMainBinding.inflate(layoutInflater)
+        val view = bindingMain.root
+        setContentView(view)
+
+        bindingMain.buttonNormal.setOnClickListener {
+            Toast.makeText(applicationContext, "Hello", Toast.LENGTH_SHORT).show()
+        }
+
+        bindingMain.buttonPrivate.setOnClickListener {
+
+            val design = layoutInflater.inflate(R.layout.toast_design, null)
+            val textViewMessage  = design.findViewById(R.id.textViewMessage) as TextView
+            textViewMessage.text = "Hello Private Message"
+
+            val toastPrivate = Toast(applicationContext)
+            toastPrivate.view = design
+            toastPrivate.setGravity(Gravity.CENTER_HORIZONTAL or Gravity.CENTER_VERTICAL, 0,0)
+            toastPrivate.duration = Toast.LENGTH_LONG
+            toastPrivate.show()
+        }
 
     }
 }
